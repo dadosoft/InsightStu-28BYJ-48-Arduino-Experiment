4c760fdbffe1d1c6cf88baffc21cc2d00c00529d
InsightStu <stefano.toto.serioli@gmail.com>
2020-02-18 09:40:27 +0100

    First shot at a toggle reverse button

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,22 @@
-/* 28BYJ-48 Speed and Reverse controller | v1.2 | by Stu */
+/* 28BYJ-48 Speed and Reverse controller | v0.2 | by Stu */
 
 #include <Arduino.h> /* Include Arduino GLibs */
 #include <Stepper.h> /* Include Stepper config */
-#include <IRRemote.h> /* Include IT libs */
+#include <IRremote.h> /* Include IT libs */
 
-const int reverser = 3;   	/* Reverse button Pin */
 const int onOffSwitch = 2;  /* Toggle Switch Pin */
+const int reverser = 3;   	/* Reverse Button Pin */
 const int shaftRevs = 32;
 const int gearRed = 64;
 const int stepsPerRev = 200; /* (shaftRevs * gearRed) is currently broken... */
 
 int stepCount = 0;
+int bState = HIGH; 
+int bPrev = LOW;
+int bCurr;
+
+unsigned long time = 0;
+unsigned long debounce = 200UL;
 
 Stepper myStepper(stepsPerRev, 8, 10, 9, 11); /* Pin 9 and 10 are reversed to make Stepper.h work with 28BYJ-48 */ 
 
@@ -21,16 +27,26 @@ void setup() {
 }
 
 void loop() {
-  
   int sensorReading = analogRead(A0);
   int motorSpeed = map(sensorReading, 0, 1023, 0, 100);
-  int reverse = digitalRead(reverser);
+  int reverse;
 
-  if (motorSpeed > 0 && reverse == HIGH) {
+  bCurr = digitalRead(reverser);
+  if (bCurr == HIGH && bPrev == LOW && millis() - time > debounce){
+    if (bState == HIGH){
+      reverse = LOW;
+    }
+    else{
+      reverse = HIGH;
+    }
+    time = millis();
+  } 
+  
+  if (motorSpeed > 0 && reverse == LOW) {
     myStepper.setSpeed(motorSpeed);
     myStepper.step(stepsPerRev / 100);
   }
-  else if (motorSpeed > 0 && reverse == LOW) {
+  else if (motorSpeed > 0 && reverse == HIGH) {
     myStepper.setSpeed(motorSpeed);
     myStepper.step(-stepsPerRev / 100);
     }
